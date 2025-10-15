f72d20dbac762d167af4ecfaae6a432d04fc9a6a
Zeprhy <lianradith@gmail.com>
2025-10-15 21:19:28 +0700

    Little UpdaTe

--- Assets/Script/UI Setup/MenuManager.cs
+++ Assets/Script/UI Setup/MenuManager.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 public class MenuManager : MonoBehaviour
 {
+    public TMP_Text panelname;
     public void LoadScene(string SceneName)
     {
-        
+        SceneManager.LoadScene(SceneName);
     }
-}
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+
+    public void setPanelName(string name)
+    {
+        panelname.text = name;
+    }
+}   
